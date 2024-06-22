e5a854eeb23866304801aa40b4f00bf474c9150f
Nathanne Isip <nathanneisip@gmail.com>
2024-06-23 01:14:54 +0800

    Initial fully-functional binary expression AST implementation.

--- src/main/java/xyz/uartix/ast/expr/BinaryExpression.java
+++ src/main/java/xyz/uartix/ast/expr/BinaryExpression.java
@@ -3,7 +3,10 @@ package xyz.uartix.ast.expr;
 import xyz.uartix.ast.ASTVisitException;
 import xyz.uartix.ast.Expression;
 import xyz.uartix.parser.Token;
+import xyz.uartix.uart.UartArithmetic;
+import xyz.uartix.util.MiscUtil;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class BinaryExpression implements Expression {
@@ -20,28 +23,52 @@ public class BinaryExpression implements Expression {
         return this.operator;
     }
 
-    public Object visit() throws ASTVisitException {
+    public Object visit() throws ASTVisitException, IOException {
         String operator = this.operator.getImage();
         Object leftValue = this.left.visit(),
             rightValue = this.right.visit();
 
         if(Objects.equals(operator, "+"))
             return switch (leftValue) {
-                case Double v when rightValue instanceof Double ->
-                    ((double) leftValue) + ((double) rightValue);
+                case Double _ when rightValue instanceof Double ->
+                    UartArithmetic.add((double) leftValue, (double) rightValue);
 
-                case Double v when rightValue instanceof String ->
+                case Double _ when rightValue instanceof String ->
                     ((double) leftValue) + rightValue.toString();
 
-                case String s when rightValue instanceof Double ->
+                case String _ when rightValue instanceof Double ->
                     leftValue.toString() + ((double) rightValue);
 
-                case String s when rightValue instanceof String ->
-                    leftValue.toString() + rightValue.toString();
+                case String _ when rightValue instanceof String ->
+                    leftValue.toString() + rightValue;
 
                 case null, default ->
                     throw new ASTVisitException(this, "Invalid binary expression operation.");
             };
+        else if(Objects.equals(operator, "*"))
+            return switch (leftValue) {
+                case Double _ when rightValue instanceof Double ->
+                    UartArithmetic.mul((double) leftValue, (double) rightValue);
+
+                case Double _ when rightValue instanceof String ->
+                    MiscUtil.multiply(rightValue.toString(), (int) (double) leftValue);
+
+                case String _ when rightValue instanceof Double ->
+                    MiscUtil.multiply(leftValue.toString(), (int) (double) rightValue);
+
+                case null, default ->
+                    throw new ASTVisitException(this, "Invalid binary expression operation.");
+            };
+        else if(Objects.equals(operator, "-")) {
+            if(leftValue instanceof Double && rightValue instanceof Double)
+                return UartArithmetic.sub((double) leftValue, (double) rightValue);
+            else throw new ASTVisitException(this, "Invalid binary expression operation.");
+        }
+        else if(Objects.equals(operator, "/")) {
+            if(leftValue instanceof Double && rightValue instanceof Double)
+                return UartArithmetic.div((double) leftValue, (double) rightValue);
+            else throw new ASTVisitException(this, "Invalid binary expression operation.");
+        }
 
         return null;
     }
