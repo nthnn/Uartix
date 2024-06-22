5b138ce093d0a3c1e1d37436f4152a2327d07b89
Nathanne Isip <nathanneisip@gmail.com>
2024-06-23 01:11:51 +0800

    Added Token as address for Node interface.

--- src/main/java/xyz/uartix/ast/Node.java
+++ src/main/java/xyz/uartix/ast/Node.java
@@ -2,7 +2,9 @@ package xyz.uartix.ast;
 
 import xyz.uartix.parser.Token;
 
+import java.io.IOException;
+
 public interface Node {
     Token getAddress();
-    Object visit() throws ASTVisitException;
+    Object visit() throws ASTVisitException, IOException;
 }
