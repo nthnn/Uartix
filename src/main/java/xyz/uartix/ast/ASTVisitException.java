0ca8294548adda386450cc28ee2cfe898d828007
Nathanne Isip <nathanneisip@gmail.com>
2024-06-24 02:25:52 +0800

    Added another constructor for ASTVisitException.

--- src/main/java/xyz/uartix/ast/ASTVisitException.java
+++ src/main/java/xyz/uartix/ast/ASTVisitException.java
@@ -1,7 +1,13 @@
 package xyz.uartix.ast;
 
+import xyz.uartix.parser.Token;
+
 public class ASTVisitException extends Exception {
     public ASTVisitException(Node node, String message) {
         super(message + " " + node.getAddress());
+    }
+
+    public ASTVisitException(Token token, String message) {
+        super(message + " " + token);
     }
 }
