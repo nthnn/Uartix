9cdead919b921a0e580879650c33da8664690383
Nathanne Isip <nathanneisip@gmail.com>
2024-06-24 02:25:34 +0800

    Added SymbolTable class to Node.visit() method.

--- src/main/java/xyz/uartix/ast/Node.java
+++ src/main/java/xyz/uartix/ast/Node.java
@@ -1,5 +1,6 @@
 package xyz.uartix.ast;
 
+import xyz.uartix.core.SymbolTable;
 import xyz.uartix.core.TerminativeSignal;
 import xyz.uartix.parser.Token;
 
@@ -7,7 +8,7 @@ import java.io.IOException;
 
 public interface Node {
     Token getAddress();
-    Object visit() throws
+    Object visit(SymbolTable symtab) throws
         ASTVisitException,
         IOException,
         TerminativeSignal;
