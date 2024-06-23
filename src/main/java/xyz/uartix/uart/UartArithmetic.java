329896959dae25bd107ae7afc0f624f48b0e294d
Nathanne Isip <nathanneisip@gmail.com>
2024-06-23 22:31:58 +0800

    Implementation of binary expressions for UART calculation.

--- src/main/java/xyz/uartix/uart/UartArithmetic.java
+++ src/main/java/xyz/uartix/uart/UartArithmetic.java
@@ -43,4 +43,74 @@ public final class UartArithmetic {
                 x, y
         );
     }
+
+    public static double and(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.AND,
+                x, y
+        );
+    }
+
+    public static double or(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.OR,
+                x, y
+        );
+    }
+
+    public static double rem(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.REM,
+                x, y
+        );
+    }
+
+    public static double pow(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.POW,
+                x, y
+        );
+    }
+
+    public static double gt(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.GT,
+                x, y
+        );
+    }
+
+    public static double ge(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.GE,
+                x, y
+        );
+    }
+
+    public static double lt(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.LT,
+                x, y
+        );
+    }
+
+    public static double le(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.LE,
+                x, y
+        );
+    }
+
+    public static double shl(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.SHL,
+                x, y
+        );
+    }
+
+    public static double shr(double x, double y) throws IOException {
+        return UartArithmetic.compute(
+                UartArithmeticCommand.SHR,
+                x, y
+        );
+    }
 }
