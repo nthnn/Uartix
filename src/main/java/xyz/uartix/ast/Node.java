a286d9e6b30e82764b723891fdeb84141cfeef5f
Nathanne Isip <nathanneisip@gmail.com>
2024-06-24 01:33:46 +0800

    Updated Node.visit() method signature with terminatory signal.

--- src/main/java/xyz/uartix/ast/Node.java
+++ src/main/java/xyz/uartix/ast/Node.java
@@ -1,10 +1,14 @@
 package xyz.uartix.ast;
 
+import xyz.uartix.core.TerminativeSignal;
 import xyz.uartix.parser.Token;
 
 import java.io.IOException;
 
 public interface Node {
     Token getAddress();
-    Object visit() throws ASTVisitException, IOException;
+    Object visit() throws
+        ASTVisitException,
+        IOException,
+        TerminativeSignal;
 }
