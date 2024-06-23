7232b23a6e30746d63758ce738fd7e71ca4cf366
Nathanne Isip <nathanneisip@gmail.com>
2024-06-24 01:37:37 +0800

    Return statement node AST.

---
+++ src/main/java/xyz/uartix/ast/stmt/ReturnStatement.java
@@ -0,0 +1,28 @@
+package xyz.uartix.ast.stmt;
+
+import xyz.uartix.ast.ASTVisitException;
+import xyz.uartix.ast.Expression;
+import xyz.uartix.ast.Statement;
+import xyz.uartix.core.TerminatoryObject;
+import xyz.uartix.core.TerminativeSignal;
+import xyz.uartix.parser.Token;
+
+import java.io.IOException;
+
+public class ReturnStatement implements Statement {
+    private final Token address;
+    private final Expression value;
+
+    public ReturnStatement(Token address, Expression value) {
+        this.address = address;
+        this.value = value;
+    }
+
+    public Token getAddress() {
+        return this.address;
+    }
+
+    public Object visit() throws TerminativeSignal, ASTVisitException, IOException {
+        throw new TerminatoryObject(this.value.visit());
+    }
+}
