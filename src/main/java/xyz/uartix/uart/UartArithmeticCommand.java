77677231d314e9a15312deab0ea9136ace4cb2e0
Nathanne Isip <nathanneisip@gmail.com>
2024-06-23 22:15:14 +0800

    Added several arithmetic command for UART.

--- src/main/java/xyz/uartix/uart/UartArithmeticCommand.java
+++ src/main/java/xyz/uartix/uart/UartArithmeticCommand.java
@@ -4,7 +4,17 @@ public enum UartArithmeticCommand {
     ADD((byte) 0x00),
     SUB((byte) 0x01),
     DIV((byte) 0x02),
-    MUL((byte) 0x03);
+    MUL((byte) 0x03),
+    AND((byte) 0x04),
+    OR((byte) 0x05),
+    POW((byte) 0x06),
+    REM((byte) 0x07),
+    GT((byte) 0x08),
+    GE((byte) 0x09),
+    LT((byte) 0x0a),
+    LE((byte) 0x0b),
+    SHL((byte) 0x0c),
+    SHR((byte) 0x0d);
 
     private final byte command;
 
