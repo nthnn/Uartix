a85afdf8d8920a49b84b0ed1d2592b264453c2a2
Nathanne Isip <nathanneisip@gmail.com>
2024-06-23 21:09:25 +0800

    Initial picoware main Arduino sketch file.

---
+++ picoware/picoware.ino
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Arduino dynaconfig library (https://github.com/nthnn/dynaconfig).
+ * Copyright (c) 2024 Nathanne Isip.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Arduino.h>
+#include "uartix.h"
+
+void setup() {
+    Serial.begin(16375000);
+    while(!Serial);
+}
+
+void loop() {
+    if(Serial.available() > 0) {
+        byte command = Serial.read();
+
+        uartix_overclock_sys();
+        uartix_actions[command]();
+        uartix_underclock_sys();
+    }
+}
