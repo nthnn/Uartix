a77f3406b12e23012c9efbdc6ee08665b61170d8
Nathanne Isip <nathanneisip@gmail.com>
2024-06-23 22:03:34 +0800

    Implemented new command signature to Uartix responses.

--- picoware/uartix.cpp
+++ picoware/uartix.cpp
@@ -99,9 +99,61 @@ inline void uartix_mul() {
     uartix_write_number(dx * dy);
 }
 
+inline void uartix_and() {
+    byte x[8], y[8];
+
+    uartix_wait_for_data(16);
+    uartix_read_number(x);
+    uartix_read_number(y);
+
+    long dx = (long) floor(uartix_b82d(x)),
+        dy = (long) floor(uartix_b82d(y));
+    uartix_write_number((double) (dx & dy));
+}
+
+inline void uartix_or() {
+    byte x[8], y[8];
+
+    uartix_wait_for_data(16);
+    uartix_read_number(x);
+    uartix_read_number(y);
+
+    long dx = (long) floor(uartix_b82d(x)),
+        dy = (long) floor(uartix_b82d(y));
+    uartix_write_number((double) (dx | dy));
+}
+
+inline void uartix_rem() {
+    byte x[8], y[8];
+
+    uartix_wait_for_data(16);
+    uartix_read_number(x);
+    uartix_read_number(y);
+
+    long dx = (long) floor(uartix_b82d(x)),
+        dy = (long) floor(uartix_b82d(y));
+    uartix_write_number((double) (dx % dy));
+}
+
+inline void uartix_pow() {
+    byte x[8], y[8];
+
+    uartix_wait_for_data(16);
+    uartix_read_number(x);
+    uartix_read_number(y);
+
+    long dx = (long) floor(uartix_b82d(x)),
+        dy = (long) floor(uartix_b82d(y));
+    uartix_write_number((double) (dx ^ dy));
+}
+
 uartix_action uartix_actions[] = {
     [UARTIX_CMD_ADD] = uartix_add,
     [UARTIX_CMD_SUB] = uartix_sub,
     [UARTIX_CMD_DIV] = uartix_div,
-    [UARTIX_CMD_MUL] = uartix_mul
+    [UARTIX_CMD_MUL] = uartix_mul,
+    [UARTIX_CMD_AND] = uartix_and,
+    [UARTIX_CMD_OR]  = uartix_or,
+    [UARTIX_CMD_POW] = uartix_pow,
+    [UARTIX_CMD_REM] = uartix_rem
 };
