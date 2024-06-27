4dd1209559d1a903ba02de296da5a9ccb42aab26
Nathanne Isip <nathanneisip@gmail.com>
2024-06-27 14:56:02 +0800

    Fixed string multiplication function on MiscUtil class.

--- src/main/java/xyz/uartix/util/MiscUtil.java
+++ src/main/java/xyz/uartix/util/MiscUtil.java
@@ -19,10 +19,6 @@ package xyz.uartix.util;
 
 public final class MiscUtil {
     public static String multiply(String string, int count) {
-        StringBuilder result = new StringBuilder();
-
-        for(int i = 0; i < count; i++)
-            result.append(string);
-        return result.toString();
+        return String.valueOf(string).repeat(Math.max(0, count));
     }
 }
