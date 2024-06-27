4e10c751f08ec790b75a0a49bde9987e076fb192
Nathanne Isip <nathanneisip@gmail.com>
2024-06-27 16:00:05 +0800

    Fully function Uartix Main class.

--- src/main/java/xyz/uartix/Main.java
+++ src/main/java/xyz/uartix/Main.java
@@ -1,11 +1,96 @@
+/*
+ * This file is part of the Uartix programming language (https://github.com/nthnn/Uartix).
+ * Copyright (c) 2024 Nathanne Isip.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package xyz.uartix;
 
+import xyz.uartix.ast.ASTVisitException;
+import xyz.uartix.ast.Statement;
+import xyz.uartix.core.SymbolTable;
+import xyz.uartix.core.TerminativeBreak;
+import xyz.uartix.core.TerminativeContinue;
+import xyz.uartix.core.TerminativeSignal;
+import xyz.uartix.core.TerminativeThrow;
+import xyz.uartix.parser.LexicalAnalysisException;
+import xyz.uartix.parser.Parser;
+import xyz.uartix.parser.ParserException;
+import xyz.uartix.uart.Uart;
 import xyz.uartix.util.RuntimeArgumentParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        RuntimeArgumentParser argParser = new RuntimeArgumentParser(args);
-        if(!argParser.parse())
-            System.exit(1);
+        try {
+            if(Uart.hasNoPortAvailable())
+                throw new IOException("No available serial port co-processor.");
+
+            RuntimeArgumentParser argParser = new RuntimeArgumentParser(args);
+            if(!argParser.parse())
+                System.exit(1);
+
+            SymbolTable symtab = new SymbolTable(null);
+            List<Statement> statements = new ArrayList<>();
+
+            for(String file : argParser.getInputFiles()) {
+                Parser parser = Parser.fromFile(file);
+                parser.parse();
+
+                statements.addAll(parser.getGlobalStatements());
+            }
+
+            Uart.connect(argParser.getSelectedPort());
+            for(Statement statement : statements)
+                statement.visit(symtab);
+        }
+        catch(LexicalAnalysisException e) {
+            System.out.println("\u001b[31mLexical Error\u001b[0m: " + e.getMessage());
+        }
+        catch(ParserException e) {
+            System.out.println("\u001b[31mParser Error\u001b[0m: " + e.getMessage());
+        }
+        catch(ASTVisitException e) {
+            System.out.println("\u001b[31mExecution Error\u001b[0m: " + e.getMessage());
+        }
+        catch(IOException e) {
+            System.out.println("\u001b[31mI/O Error\u001b[0m: " + e.getMessage());
+        }
+        catch(RuntimeException e) {
+            System.out.println("\u001b[31mRuntime Error\u001b[0m: " + e.getMessage());
+        }
+        catch(TerminativeSignal e) {
+            if(e instanceof TerminativeBreak ||
+                e instanceof TerminativeContinue)
+                System.out.println("\u001b[31mTerminative signal received.\u001b[0m");
+            else if(e instanceof TerminativeThrow) {
+                Object obj = ((TerminativeThrow) e).getObject();
+                if(obj == null)
+                    obj = "nil";
+
+                System.out.println("\u001b[31mUncaught error\u001b[0m: " + obj);
+            }
+        }
+
+        try {
+            Uart.disconnect();
+        }
+        catch(IOException e) {
+            System.out.println("\u001b[31mI/O Error\u001b[0m: " + e.getMessage());
+        }
     }
 }
