1f2f4be1364543f5556bc6e67c856bd1330b026a
Nathanne Isip <nathanneisip@gmail.com>
2024-06-27 14:59:52 +0800

    Improved field types for while expression AST.

--- src/main/java/xyz/uartix/ast/expr/WhileExpression.java
+++ src/main/java/xyz/uartix/ast/expr/WhileExpression.java
@@ -19,7 +19,6 @@ package xyz.uartix.ast.expr;
 
 import xyz.uartix.ast.ASTVisitException;
 import xyz.uartix.ast.Expression;
-import xyz.uartix.ast.Statement;
 import xyz.uartix.core.*;
 import xyz.uartix.parser.Token;
 
