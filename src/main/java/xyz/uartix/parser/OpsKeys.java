36a1277b97f7c066e2967c46a4a6d562fe2de2bf
Nathanne Isip <nathanneisip@gmail.com>
2024-08-01 02:40:52 +0800

    Added keywords for test and use statement.

--- src/main/java/xyz/uartix/parser/OpsKeys.java
+++ src/main/java/xyz/uartix/parser/OpsKeys.java
@@ -21,24 +21,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public final class OpsKeys {
-    public static List<String> operators = Arrays.asList(
-        "+", "-", "*", "/",
-        "!", "!=", "&", "&&",
-        "|", "||", "^", "%",
-        "(", ")", "[", "]",
-        "{", "}", "=", "==",
-        ":", ";", "'", "\"",
-        "<", "<<", "<=", ">",
-        ">>", ">=", ",", ".",
-        "::", "?"
-    );
+    public static List<String> operators = Arrays.asList("+", "-", "*", "/", "!", "!=", "&", "&&", "|", "||", "^", "%", "(", ")", "[", "]", "{", "}", "=", "==", ":", ";", "'", "\"", "<", "<<", "<=", ">", ">>", ">=", ",", ".", "::", "?");
 
-    public static List<String> keywords = Arrays.asList(
-        "while", "do", "loop", "render",
-        "unless", "if", "else", "when",
-        "break", "continue", "random",
-        "func", "ret", "true", "false",
-        "nil", "catch", "handle", "throw",
-        "maybe", "then", "type"
-    );
+    public static List<String> keywords = Arrays.asList("while", "do", "loop", "render", "unless", "if", "else", "when", "break", "continue", "random", "func", "ret", "true", "false", "nil", "catch", "handle", "throw", "maybe", "then", "type", "use", "test");
 }
